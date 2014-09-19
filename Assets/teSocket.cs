6933fd1554b3e339f79534b0c1ba4a9c167974b1
Pablo Pernias <pablo@pernias.com>
2014-09-19 19:46:21 +0200

    added first socket.io tests

---
+++ Assets/teSocket.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class teSocket : MonoBehaviour {
+
+	SocketIOClient.Client socket;
+	// Use this for initialization
+	void Start () {
+		socket = new SocketIOClient.Client("http://localhost:3000/");
+
+		socket.On("connect", (fn) => {
+			Debug.Log ("connect - socket");
+			
+			Dictionary<string, string> args = new Dictionary<string, string>();
+			args.Add("msg", "what's up?");
+			socket.Emit("SEND", args);
+		});
+
+		socket.Connect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnApplicationQuit(){
+		socket.Close();
+	}
+}
---
+++ socketio/server.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server, { log: false });
+
+app.get('/', function(req, res){
+    res.send("it works");
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('SEND', function(msg){
+        console.log('message: ' + JSON.stringify(msg));
+    });
+
+});
+
+server.listen(3000, function(){
+    console.log('listening on *:3000');
+});
